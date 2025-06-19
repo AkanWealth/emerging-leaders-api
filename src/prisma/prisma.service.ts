6165387fe715c380909d43de1ce561a7781cd796
Paschal <paschalnwokeocha1@gmail.com>
2025-06-19 18:56:19 +0100

    prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,31 +1,9 @@
-import { Injectable, OnModuleInit, OnModuleDestroy, INestApplication } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma?: PrismaService };
-
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    if (globalForPrisma.prisma) return globalForPrisma.prisma;
-
-    super({
-      log: ['warn', 'error'],
-    });
-
-    if (process.env.NODE_ENV !== 'production') {
-      globalForPrisma.prisma = this;
-    }
-  }
-
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
-
-  async enableShutdownHooks(app: INestApplication) {
-    app.enableShutdownHooks();
   }
 }
