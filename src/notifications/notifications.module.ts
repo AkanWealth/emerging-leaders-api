5f5da3670ec5fcae2c1f46bf574611a7d5c42e36
Paschal <paschalnwokeocha1@gmail.com>
2025-06-23 16:35:40 +0100

    final

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,4 +1,9 @@
+// src/notifications/notifications.module.ts
 import { Module } from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
 
-@Module({})
+@Module({
+  providers: [NotificationsService],
+  exports: [NotificationsService], // 👈 make it exportable
+})
 export class NotificationsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],  // <=== VERY important to export it
