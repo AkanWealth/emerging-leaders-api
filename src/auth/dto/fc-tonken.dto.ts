2597d502f91a1daf2a2c60939ba8031ca36ac2a1
paschal1 <paschalnwokeocha1@gmail.com>
2025-10-13 08:39:36 +0100

    fix

--- src/auth/dto/fc-tonken.dto.ts
+++ src/auth/dto/fc-tonken.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class RegisterFcmTokenDto {
   @ApiProperty({ example: 'abcd1234', description: 'Firebase Cloud Messaging token' })
+  @IsString()
   token: string;
 
   @ApiProperty({ example: 'android', description: 'Platform type (android, ios, web)' })
+  @IsString()
   platform: string;
 }
