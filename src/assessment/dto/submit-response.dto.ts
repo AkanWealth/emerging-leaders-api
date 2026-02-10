5803c51f3711aa768cc69e1e149cf0b9b98a0bcc
paschal1 <paschalnwokeocha1@gmail.com>
2026-02-10 18:51:24 +0100

    cuid fixed

--- src/assessment/dto/submit-response.dto.ts
+++ src/assessment/dto/submit-response.dto.ts
@@ -1,19 +1,20 @@
-import { IsUUID, IsObject } from 'class-validator';
+import { IsString, IsObject } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SubmitAssessmentResponseDto {
   @ApiProperty({
-    description: 'UUID of the assessment being submitted',
-    example: '3f5a1e34-8b21-4f43-b5c3-8c2c7f4d72af',
+    description: 'CUID of the assessment being submitted',
+    example: 'cmlgr5hu90001uad01d4nwcoi',
   })
-  @IsUUID()
+  @IsString()
   assessmentId: string;
 
   @ApiProperty({
-    description: 'Answers to the assessment questions. Key is questionId (UUID), value is the submitted answer.',
+    description:
+      'Answers to the assessment questions. Key is questionId (CUID), value is the submitted answer.',
     example: {
-      'a9c3b11f-4b25-491a-bb3f-5d1b7c2e9a10': 'JavaScript',
-      'b2f9e512-88e7-4e02-a6c2-ded22f3e8c10': ['Node.js', 'React'],
+      cmlgq2abc0002uad0xyz1234: 'JavaScript',
+      cmlgq2def0003uad0xyz5678: ['Node.js', 'React'],
     },
   })
   @IsObject()
